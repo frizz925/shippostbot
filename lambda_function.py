5cf3e9b2408b046fb88cf4cb66c29dd4b3ea4e00
Izra Faturrahman <Frizz925@hotmail.com>
2019-05-13 07:42:20 +0700

    Add env encryption switch

--- lambda_function.py
+++ lambda_function.py
@@ -8,11 +8,23 @@ import shippostbot
 
 def lambda_handler(event, context):
     try:
-        crypto = CryptoHelper()
-        res = shippostbot.main(crypto.env('S3_REGION'),
-                               crypto.env('S3_BUCKET_NAME'),
-                               crypto.env('FACEBOOK_ACCESS_TOKEN'),
-                               crypto.env('FACEBOOK_PAGE_ID'))
+        region = os.environ['S3_REGION']
+        bucket_name = os.environ['S3_BUCKET_NAME']
+        access_token = os.environ['FACEBOOK_ACCESS_TOKEN']
+        page_id = os.environ['FACEBOOK_PAGE_ID']
+
+        is_encrypted_env = os.environ.get('ENCRYPTED_ENV', False) == 'true'
+        if is_encrypted_env:
+            crypto = CryptoHelper()
+            res = shippostbot.main(crypto.decrypt(region),
+                                   crypto.decrypt(bucket_name),
+                                   crypto.decrypt(access_token),
+                                   crypto.decrypt(page_id))
+        else:
+            res = shippostbot.main(region,
+                                   bucket_name,
+                                   access_token,
+                                   page_id)
         return {
             'statusCode': 200,
             'data': res.json()
@@ -28,7 +40,6 @@ class CryptoHelper(object):
     def __init__(self):
         self.kms = boto3.client('kms')
 
-    def env(self, name: str) -> str:
-        encrypted = os.environ.get(name)
+    def decrypt(self, encrypted: str) -> str:
         decrypted = self.kms.decrypt(CiphertextBlob=b64decode(encrypted))['Plaintext']
         return decrypted
