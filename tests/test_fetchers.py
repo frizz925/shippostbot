41a82de34ad56fa3b82957e0ba325ae6c48fb4a7
Izra Faturrahman <Frizz925@hotmail.com>
2019-05-22 08:05:20 +0700

    Add single object fetching test

--- tests/test_fetchers.py
+++ tests/test_fetchers.py
@@ -1,4 +1,18 @@
-from shippostbot.fetchers import fetch_random_character, fetch_random_media
+from shippostbot.fetchers import (fetch_character, fetch_media,
+                                  fetch_random_character, fetch_random_media)
+
+
+def test_fetch_media():
+    media = fetch_media(21519)
+    assert isinstance(media, dict)
+    assert media['title']['userPreferred'] == 'Kimi no Na wa.'
+
+
+def test_fetch_character():
+    chara = fetch_character(60153)
+    assert isinstance(chara, dict)
+    assert chara['name']['last'] == 'Senomiya'
+    assert chara['name']['first'] == 'Akiho'
 
 
 def test_random_media():
