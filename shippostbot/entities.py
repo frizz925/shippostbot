49974aa70875090174b17690d5d0b2ef62c6b607
Izra Faturrahman <Frizz925@hotmail.com>
2019-06-15 23:35:44 +0700

    Restructure the entities

--- shippostbot/entities.py
+++ shippostbot/entities.py
@@ -1,7 +1,7 @@
 import collections
 
-Media = collections.namedtuple('Media', 'title')
+Media = collections.namedtuple('Media', 'title url')
 
-Character = collections.namedtuple('Character', 'first_name last_name image_url')
+Character = collections.namedtuple('Character', 'first_name last_name image_url url')
 
-Post = collections.namedtuple('Post', 'media caption first_character second_character')
+Post = collections.namedtuple('Post', 'characters media caption comment')
