9232fc228ef07c34a64356d627811990c8a5c1c5
Izra Faturrahman <Frizz925@hotmail.com>
2019-06-20 04:53:07 +0700

    Load dotenv file first before reading env

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ from dotenv import load_dotenv
 import shippostbot
 
 if __name__ == "__main__":
+    dotenv_path = path.join(path.dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+
     if os.environ.get('DEBUG_IMAGE') == 'true':
         shippostbot.image.DEBUG_IMAGE = True
     if os.environ.get('DEBUG_LOG') == 'true':
         shippostbot.log.STDOUT_LEVEL = logging.DEBUG
 
-    dotenv_path = path.join(path.dirname(__file__), '.env')
-    load_dotenv(dotenv_path)
     shippostbot.main(os.environ.get('S3_REGION'),
                      os.environ.get('S3_BUCKET_NAME'),
                      os.environ.get('FACEBOOK_ACCESS_TOKEN'),
