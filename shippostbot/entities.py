95baa230bca57302093e415078fc3197835a6c4c
Izra Faturrahman <Frizz925@hotmail.com>
2019-06-22 01:05:17 +0700

    Improve typing

--- shippostbot/entities.py
+++ shippostbot/entities.py
@@ -1,7 +1,26 @@
-import collections
+from __future__ import annotations
 
-Media = collections.namedtuple('Media', 'id title url characters')
+from typing import List, NamedTuple
 
-Character = collections.namedtuple('Character', 'id first_name last_name image_url url media')
 
-Post = collections.namedtuple('Post', 'characters media caption comment')
+class Media(NamedTuple):
+    id: int
+    title: str
+    url: str
+    characters: List[Character]
+
+
+class Character(NamedTuple):
+    id: int
+    first_name: str
+    last_name: str
+    image_url: str
+    url: str
+    media: List[Media]
+
+
+class Post(NamedTuple):
+    characters: List[Character]
+    media: List[Media]
+    caption: str
+    comment: str
