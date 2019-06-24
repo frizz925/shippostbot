64f1a98b2b17a3800cd33fa00e6d3bbc0b814f77
Izra Faturrahman <Frizz925@hotmail.com>
2019-06-24 08:32:20 +0700

    Add logging for Facebook publisher

--- shippostbot/social/facebook_publisher.py
+++ shippostbot/social/facebook_publisher.py
@@ -1,5 +1,6 @@
 from typing import NamedTuple, Type
 
+from ..log import create_logger
 from ..photo import upload_photo
 from ..storage.abstracts import Storage
 from .abstracts import Photo, Publisher
@@ -28,6 +29,7 @@ class FacebookPublisher(Publisher):
                  storage: Type[Storage]):
         self.api = api
         self.user_api = api.get_user()
+        self.logger = create_logger(FacebookPublisher)
 
         if not storage.is_public:
             raise Exception('Provided storage must be public')
@@ -42,16 +44,25 @@ class FacebookPublisher(Publisher):
 
     def publish_photo(self, photo: Photo) -> FacebookPost:
         f = upload_photo(self.storage, photo)
+        self.logger.info('publish_photo() start, ' +
+                         'caption: "' + photo.caption +
+                         '", image_url: "' + f.public_url +
+                         '", content_type: "' + photo.image.content_type + '"')
         res = self.user_api.publish_photo(photo.caption, f.public_url)
         f.delete()
-        res.raise_for_status()
         res_json = res.json()
+        self.logger.info('publish_photo() result: ' + res_json)
+        res.raise_for_status()
         return FacebookPost(id=res_json['post_id'],
                             user_id=res_json['id'])
 
     def publish_comment(self, post_id: str, content: str) -> FacebookComment:
+        self.logger.info('publish_comment() start, ' +
+                         'post_id: "' + post_id +
+                         '", content: "' + content + '"')
         res = self.api.publish_comment(post_id, content)
-        res.raise_for_status()
         res_json = res.json()
+        self.logger.info('publish_comment() result: ' + res_json)
+        res.raise_for_status()
         return FacebookComment(id=res_json['id'],
                                post_id=post_id)
