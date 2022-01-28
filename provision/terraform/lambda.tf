978d719ee9e1fda5f16fc58dad89dec86f0c5324
Izra Faturrahman <Frizz925@hotmail.com>
2022-01-28 18:36:22 +0900

    Upgrade to Python 3.10

--- provision/terraform/lambda.tf
+++ provision/terraform/lambda.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_function" "shippostbot_lambda" {
   filename = "../artifacts/shippostbot.zip"
   source_code_hash = "${filebase64sha256("../artifacts/shippostbot.zip")}"
 
-  runtime = "python3.7"
+  runtime = "python3.10"
   memory_size = 128
   timeout = 300
 
@@ -35,7 +35,7 @@ resource "aws_lambda_function" "shippostbot_lambda" {
 
 resource "aws_lambda_layer_version" "shippostbot_dependencies" {
   layer_name = "ShippostBotDependencies"
-  compatible_runtimes = ["python3.7"]
+  compatible_runtimes = ["python3.10"]
   license_info = "GPL-3.0-or-later"
 
   filename = "../artifacts/shippostbot-deps.zip"
